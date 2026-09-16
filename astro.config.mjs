47dc215e1a5834a3570e962908efe5a09fab5bca
Gabriel Escobar <gabrielescobar283@gmail.com>
2026-09-15 22:43:55 -0500

    fix: habilitar servicio de imagenes nativo de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
-import { defineConfig, sharpImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
+  adapter: vercel({
+    imageService: true,
+  }),
   vite: {
     plugins: [tailwindcss()]
-  },
-  image:{
-    service: sharpImageService(),
   }
 });
